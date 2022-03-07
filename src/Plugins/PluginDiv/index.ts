3515d1f1bd9eb4240fe12da71f3b36ebfe3db9cc
Siddhesh Mangela <sidthecool007@gmail.com>
2022-03-07 11:33:51 +0530

    PluginDiv type added

--- src/Plugins/PluginDiv/index.ts
+++ src/Plugins/PluginDiv/index.ts
@@ -2,6 +2,7 @@ import PluginDivComponent from './PluginDiv.svelte';
 
 const PluginDiv = {
   id: 'plugin-div',
+  type: 'text',
   name: 'Div',
   description: 'Plugin Div. This is a description for this plugin. This is simple div',
   component: PluginDivComponent,
